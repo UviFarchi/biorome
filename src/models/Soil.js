08c581af3a724400515d4b1fd25ea410c4e11f2a
UviFarchi <uvi@modulardynamics.com>
2025-07-03 22:39:36 +0200

    Add model factories for Plant, Animal and Soil

--- src/models/Soil.js
+++ src/models/Soil.js
@@ -1,5 +1,17 @@
-export default class Soil {
-  constructor(type = 'Soil') {
-    this.type = type
+// Factory for creating Soil objects
+
+export function createSoil(options = {}) {
+  const {
+    fertility = 1,
+    health = { current: 100, max: 100 },
+    recoveryRate = 1,
+    tilePos = { row: null, col: null }
+  } = options
+
+  return {
+    fertility,
+    health: { ...health },
+    recoveryRate,
+    tilePos: { ...tilePos }
   }
 }
